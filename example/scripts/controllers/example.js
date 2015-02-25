9b763d702315dd177b44b67cde493ee73fcd302b
Mike McAuley <mcauley86@gmail.com>
2015-02-25 13:08:03 -0600

    new data for table in example page

--- example/scripts/controllers/example.js
+++ example/scripts/controllers/example.js
@@ -2,9 +2,84 @@ angular.module('fixtableExample').controller('ExampleCtrl', [
   '$scope', function($scope) {
     $scope.data = [
       {
-        name: 'george',
-        color: 'blue',
-        description: 'here is a description'
+        year: 2015,
+        film: 'Birdman or (The Unexpected Virtue of Ignorance)',
+        director: 'Alejandro González Iñárritu'
+      },
+      {
+        year: 2014,
+        film: '12 Years a Slave',
+        director: 'Steve McQueen'
+      },
+      {
+        year: 2013,
+        film: 'Argo',
+        director: 'Ben Affleck'
+      },
+      {
+        year: 2012,
+        film: 'The Artist',
+        director: 'Michel Hazanavicius'
+      },
+      {
+        year: 2011,
+        film: 'The King\'s Speech',
+        director: 'Tom Hooper'
+      },
+      {
+        year: 2010,
+        film: 'The Hurt Locker',
+        director: 'Kathryn Bigelow'
+      },
+      {
+        year: 2009,
+        film: 'Slumdog Millionaire',
+        director: 'Danny Boyle, Loveleen Tandan'
+      },
+      {
+        year: 2008,
+        film: 'No Country For Old Men',
+        director: 'Joel Coen, Ethan Coen'
+      },
+      {
+        year: 2007,
+        film: 'The Departed',
+        director: 'Martin Scorsese'
+      },
+      {
+        year: 2006,
+        film: 'Crash',
+        director: 'Paul Haggis'
+      },
+      {
+        year: 2005,
+        film: 'Million Dollar Baby',
+        director: 'Clint Eastwood'
+      },
+      {
+        year: 2004,
+        film: 'Lord Of The Rings: Return Of The King',
+        director: 'Peter Jackson'
+      },
+      {
+        year: 2003,
+        film: 'Chicago',
+        director: 'Rob Marshall'
+      },
+      {
+        year: 2002,
+        film: 'A Beautiful Mind',
+        director: 'Ron Howard'
+      },
+      {
+        year: 2001,
+        film: 'Gladiator',
+        director: 'Ridley Scott'
+      },
+      {
+        year: 2000,
+        film: 'American Beauty',
+        director: 'Sam Mendes'
       }
     ];
     $scope.addItem = function() {
@@ -18,16 +93,16 @@ angular.module('fixtableExample').controller('ExampleCtrl', [
       data: 'data',
       columns: [
         {
-          property: 'name',
-          label: 'Name',
+          property: 'year',
+          label: 'Year',
           width: 50
         }, {
-          property: 'color',
-          label: 'Color',
+          property: 'film',
+          label: 'Film',
           width: 200
         }, {
-          property: 'description',
-          label: 'Description',
+          property: 'director',
+          label: 'Director',
           width: 100
         }
       ]
